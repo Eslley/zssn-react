52e45a3215d9d7ed936815ef17c8e4879aaabc52
Eslley <rubemeslley@gmail.com>
2022-11-02 22:20:23 -0300

    #1 Informando alerta de contaminação

--- src/pages/sobreviventes/Sobreviventes.js
+++ src/pages/sobreviventes/Sobreviventes.js
@@ -1,4 +1,4 @@
-import { Button, Fab, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Fab, FormControl, IconButton, InputLabel, MenuItem, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Tooltip } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import { ReportProblem, EditLocation, People, Add } from '@mui/icons-material/';
 import zombie from '../../img/zombie.png'
@@ -18,15 +18,20 @@ const nameButtonStyles = {
 function Sobreviventes() {
 
   const [sobreviventes, setSobreviventes] = useState([])
+  const [idInformante, setIdInformante] = useState('')
+  const [idContaminado, setIdContaminado] = useState('')
+
+  const [ selectSobreviventes, setSelectSobreviventes ] = useState([])
 
   const [openForm, setOpenForm] = useState(false)
   const [detailsOpen, setDetailsOpen] = useState(false)
+  const [infeccaoOpen, setInfeccaoOpen] = useState(false)
 
   const { startLoader, stopLoader } = useLoader()
 
   const [sobrevivente, setSobrevivente] = useState({})
 
-  const { showAlert }  = useAlertMessage()
+  const { showAlert } = useAlertMessage()
 
   useEffect(() => {
 
@@ -60,6 +65,51 @@ function Sobreviventes() {
 
   function handleOpenForm() {
     setOpenForm(true)
+  }
+
+  function openAlertaInfeccao(idContaminado) {
+    const select = sobreviventes.filter((s) => !s.estaInfectado && s.id != idContaminado)
+    console.log(select)
+    setSelectSobreviventes(select)
+    setInfeccaoOpen(true)
+    setIdContaminado(idContaminado)
+  }
+
+  function closeAlertaInfeccao() {
+    setInfeccaoOpen(false)
+  }
+
+  function submitAlerta(e) {
+    startLoader()
+    e.preventDefault()
+    
+    sobreviventesService.alertInfected(idInformante, idContaminado)
+      .then(res => {
+
+        if(res.status === 200) {
+
+          showAlert('', res.data.message, 'success', 4000)
+
+          listarSobreviventes()
+          setInfeccaoOpen(false)
+        }
+
+        stopLoader()
+
+      })
+      .catch(err => {
+        if(!!err.message) {
+          showAlert('', err.message, 'info', 5000)
+        }
+
+        console.log(err)
+        stopLoader()
+      })
+
+  }
+
+  function handleInformanteChange(e) {
+    setIdInformante(e.target.value)
   }
 
   return (
@@ -96,17 +146,17 @@ function Sobreviventes() {
                   <TableCell align="center">
                     <Tooltip title="Editar localização" arrow>
                       <span>
-                      <IconButton disabled={sobrevivente.estaInfectado} aria-label="delete" color="primary">
-                        <EditLocation />
-                      </IconButton>
+                        <IconButton disabled={sobrevivente.estaInfectado} aria-label="delete" color="primary">
+                          <EditLocation />
+                        </IconButton>
                       </span>
                     </Tooltip>
 
                     <Tooltip title="Alertar infecção" arrow>
                       <span>
-                      <IconButton disabled={sobrevivente.estaInfectado} aria-label="report" color="error">
-                        <ReportProblem />
-                      </IconButton>
+                        <IconButton onClick={() => openAlertaInfeccao(sobrevivente.id)} disabled={sobrevivente.estaInfectado} aria-label="report" color="error">
+                          <ReportProblem />
+                        </IconButton>
                       </span>
                     </Tooltip>
                   </TableCell>
@@ -138,6 +188,34 @@ function Sobreviventes() {
 
       {/* Component que exibe detalhes do sobrevivente */}
       <SobreviventeDetails sobrevivente={sobrevivente} detailsOpen={detailsOpen} setDetailsOpen={setDetailsOpen} />
+
+
+      {/* Modal para informar infecção */}
+      <Dialog open={infeccaoOpen} onClose={closeAlertaInfeccao}>
+        <DialogTitle>Alerta de Contaminação</DialogTitle>
+        <form onSubmit={submitAlerta}>
+          <DialogContent>
+            <FormControl fullWidth>
+              <InputLabel id="select-label">Informante</InputLabel>
+              <Select
+                labelId="select-label"
+                id="select"
+                value={idInformante}
+                label="Informante"
+                onChange={handleInformanteChange}
+              >
+                {selectSobreviventes.map((sobrevivente, index) => 
+                  <MenuItem key={index} value={sobrevivente.id}>{sobrevivente.nome}</MenuItem> 
+                )}
+              </Select>
+            </FormControl>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={closeAlertaInfeccao}>Cancelar</Button>
+            <Button type="submit">Confirmar</Button>
+          </DialogActions>
+        </form>
+      </Dialog>
 
     </>
   );
